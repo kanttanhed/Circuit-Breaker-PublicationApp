f49394832e63d2df6c6c1b449f8fc2914e6b4e60
Plinio <plinio.salazar@yahoo.com.br>
2024-01-07 11:37:28 +0000

    feat: Initial Project

---
+++ publication-service/src/test/java/com/kanttanhed/Publication/PublicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanttanhed.Publication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
