990855d998fbe679d463b4d32997418911b5776a
Plinio <plinio.salazar@yahoo.com.br>
2024-01-21 19:10:27 +0000

    Feat: Implementing Log configuration status change

---
+++ publication-service/src/main/java/com/kanttanhed/Publication/config/CircuitBreakerConfig.java
@@ -0,0 +1,34 @@
+package com.kanttanhed.Publication.config;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.core.registry.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class CircuitBreakerConfig {
+
+    @Bean
+    public RegistryEventConsumer<CircuitBreaker> getLog(){
+        return new RegistryEventConsumer<CircuitBreaker>() {
+            @Override
+            public void onEntryAddedEvent(EntryAddedEvent<CircuitBreaker> entryAddedEvent) {
+                entryAddedEvent.getAddedEntry()
+                        .getEventPublisher()
+                        .onStateTransition(event -> log.info(event.toString()));
+            }
+
+            @Override
+            public void onEntryRemovedEvent(EntryRemovedEvent<CircuitBreaker> entryRemoveEvent) {
+
+            }
+
+            @Override
+            public void onEntryReplacedEvent(EntryReplacedEvent<CircuitBreaker> entryReplacedEvent) {
+
+            }
+        };
+    }
+}
