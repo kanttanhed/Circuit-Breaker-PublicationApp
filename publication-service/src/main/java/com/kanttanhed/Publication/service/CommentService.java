15a215a8435a3001e084bf6c3efd02d052e3a51d
Plinio <plinio.salazar@yahoo.com.br>
2024-01-26 23:57:50 +0000

    Feat: Implementing CommentService

---
+++ publication-service/src/main/java/com/kanttanhed/Publication/service/CommentService.java
@@ -0,0 +1,29 @@
+package com.kanttanhed.Publication.service;
+
+import com.kanttanhed.Publication.client.CommentClient;
+import com.kanttanhed.Publication.domain.Comment;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class CommentService {
+
+    @Autowired
+    private CommentClient commentClient;
+
+    @CircuitBreaker(name = "comments", fallbackMethod = "getCommentsFallback")
+    public List<Comment> getComments(String id) {
+        return commentClient.getComments(id);
+    }
+
+    private List<Comment> getCommentsFallback(String id, Throwable cause) {
+        log.warn("[WARN] fallback with id {}", id);
+        return List.of();
+    }
+}
+
